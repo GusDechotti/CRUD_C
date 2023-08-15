829376915a9f2317b5d750b513d37df609e2bfb4
Gustavo Dechotti <75210025+GusDechotti@users.noreply.github.com>
2023-08-15 16:00:18 -0300

    headers

---
+++ cadastro.h
@@ -0,0 +1,7 @@
+int login(){
+ char nome[50],tel[18]/*+55(45)90000-0000*/,rg[13]/*00.000.000-0*/;
+    printf("--FACA SEU CADASTRO:--\n Digite seu nome completo:\n");
+
+
+    printf("Se vc esta lendo isso significa que deu certo");
+    }
