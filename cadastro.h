2a8122238bf82e4d0960088c0fd9fb69e39982b0
Gustavo Dechotti <75210025+GusDechotti@users.noreply.github.com>
2023-09-22 12:08:52 -0300

    Update cadastro.h

--- cadastro.h
+++ cadastro.h
@@ -1,7 +1,1 @@
-int login(){
- char nome[50],tel[18]/*+55(45)90000-0000*/,rg[13]/*00.000.000-0*/;
-    printf("--FACA SEU CADASTRO:--\n Digite seu nome completo:\n");
 
-
-    printf("Se vc esta lendo isso significa que deu certo");
-    }
